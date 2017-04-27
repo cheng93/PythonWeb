a0bb7f798c4ee58fd34687a46efcdbbe03170c34
cheng93 <derek.c@hotmail.co.uk>
2017-04-27 20:00:47 +0100

    init_db() and rename views to controllers

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,11 +1,13 @@
 import os
 import sys
 
+from app.data.database import init_db
 from flask import Flask, blueprints
 
 app = Flask(__name__)
+init_db()
 
-from app.api.films.views import films  # noqa
+from app.api.films.controllers import films  # noqa
 
 app.register_blueprint(films)
 
