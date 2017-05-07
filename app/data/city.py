c9fe8d27cfc61fe053f5380885af960b3890de08
cheng93 <derek.c@hotmail.co.uk>
2017-05-07 22:24:00 +0100

    fix FK

--- app/data/city.py
+++ app/data/city.py
@@ -9,7 +9,7 @@ class City(Base):
 
     city_id = Column(Integer, primary_key=True, server_default=text("nextval('city_city_id_seq'::regclass)"))
     city = Column(String(50), nullable=False)
-    country_id = Column(ForeignKey('country.py.country_id'), nullable=False, index=True)
+    country_id = Column(ForeignKey('country.country_id'), nullable=False, index=True)
     last_update = Column(DateTime, nullable=False, server_default=text("now()"))
 
     country = relationship('Country')
