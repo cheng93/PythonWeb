1c0f11edfa90b5ec0a5d1d39f65b98c30d6e0af3
cheng93 <derek.c@hotmail.co.uk>
2017-05-07 22:24:27 +0100

    move data initalization into data.__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,11 @@
-from app.data import initialize_db
-
 from pyramid.config import Configurator
-
-from sqlalchemy import engine_from_config
 
 
 def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.scan('app.data')
-    engine = engine_from_config(settings, 'sqlalchemy.')
-    initialize_db(engine)
     config.include('pyramid_chameleon')
+    config.include('.data')
     config.include('.web')
 
     return config.make_wsgi_app()
--- app/data/__init__.py
+++ app/data/__init__.py
@@ -1,11 +1,35 @@
+from sqlalchemy import engine_from_config
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.orm import sessionmaker, configure_mappers
 
-DBSession = scoped_session(sessionmaker())
+import zope.sqlalchemy
+
 Base = declarative_base()
 
+configure_mappers()
 
-def initialize_db(engine):
-    DBSession.configure(bind=engine)
-    Base.metadata.bind = engine
-    Base.metadata.create_all(engine)
+
+def get_session_factory(engine):
+    factory = sessionmaker()
+    factory.configure(bind=engine)
+    return factory
+
+
+def get_tm_session(session_factory, transaction_manager):
+    session = session_factory()
+    zope.sqlalchemy.register(session, transaction_manager=transaction_manager)
+    return session
+
+
+def includeme(config):
+    settings = config.get_settings()
+
+    config.include('pyramid_tm')
+
+    engine = engine_from_config(settings, 'sqlalchemy.')
+    session_factory = get_session_factory(engine)
+
+    config.add_request_method(
+        lambda r: get_tm_session(session_factory, r.tm),
+        'db',
+        reify=True)
