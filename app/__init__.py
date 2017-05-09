ae82af20a5ffe5feb4e65a1f8820eb3ada965bad
cheng93 <derek.c@hotmail.co.uk>
2017-05-09 19:35:27 +0100

    support for environment variables..ish

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,20 @@
 import app.data
 import app.web
 
+import os
+
 import pyramid_chameleon
 from pyramid.config import Configurator
 
 
+def expandvars_dict(settings):
+    """Expands all environment variables in a settings dictionary."""
+    return dict((key, os.path.expandvars(value)) for
+                key, value in settings.items())
+
+
 def main(global_config, **settings):
+    settings = expandvars_dict(settings)
     config = Configurator(settings=settings)
     config.scan(app.data)
     config.include(pyramid_chameleon)
