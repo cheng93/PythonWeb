3a25f0da8dd3301ec5f5ce947c46afc918fbcba4
cheng93 <derek.c@hotmail.co.uk>
2017-05-10 20:33:07 +0100

    support for templating

--- app/logging.py
+++ app/logging.py
@@ -1,10 +1,23 @@
 import structlog
 from structlog.processors import JSONRenderer
+from structlog.stdlib import add_log_level
 from structlog.threadlocal import wrap_dict
 
 
+class __SafeDict(dict):
+    def __missing__(self, key):
+        return f'{{{key}}}'
+
+
+def __populate_template(_, __, event_dict):
+    event_dict['template'] = event_dict['event']
+    event_dict['event'] = event_dict['event'].format_map(__SafeDict(event_dict))
+    return event_dict
+
 structlog.configure(
         processors=[
+            add_log_level,
+            __populate_template,
             JSONRenderer(indent=2)
         ],
         context_class=wrap_dict(dict))
