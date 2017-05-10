536ba0b4fb5373ca8e63818434ffd48f6098330d
cheng93 <derek.c@hotmail.co.uk>
2017-05-10 20:59:06 +0100

    include referrer and matched_route info in log

--- app/tweens/logging_tween.py
+++ app/tweens/logging_tween.py
@@ -16,7 +16,8 @@ class logging_tween_factory(object):
             request_host=request.host,
             request_path=request.path,
             request_query_string=request.query_string,
-            server_protocol=request.environ['SERVER_PROTOCOL'])
+            server_protocol=request.environ['SERVER_PROTOCOL'],
+            referer=request.referer)
 
         start_time = default_timer()
 
@@ -27,7 +28,9 @@ class logging_tween_factory(object):
         request.logger.bind(
             elapsed='{:.4f}'.format(end_time),
             status_code=response.status_int,
-            content_type=response.content_type)
+            content_type=response.content_type,
+            matched_route_name=request.matched_route.name,
+            matched_route_pattern=request.matched_route.pattern)
 
         request.logger.info(MESSAGE_TEMPLATE)
 
