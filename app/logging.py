f80d7e40fc8547d016bcbc0e6fa77f3734380423
cheng93 <derek.c@hotmail.co.uk>
2017-05-11 21:25:37 +0100

    finish the logging tween

--- app/logging.py
+++ app/logging.py
@@ -14,31 +14,17 @@ def __populate_template(_, __, event_dict):
     return event_dict
 
 structlog.configure(
-        processors=[
-            add_log_level,
-            __populate_template,
-            JSONRenderer(indent=2)
-        ],
-        context_class=dict,
-        logger_factory=LoggerFactory())
-
-
-def logger_factory(request):
-    logger = structlog.get_logger()
-    logger.bind(
-        http_user_agent=request.headers.environ['HTTP_USER_AGENT'],
-        server_protocol=request.environ['SERVER_PROTOCOL'],
-        request_method=request.method,
-        request_url=request.url,
-        request_host=request.host,
-        request_path=request.path,
-        request_query_string=request.query_string,
-        referer=request.referer)
-    return logger
+    processors=[
+        add_log_level,
+        __populate_template,
+        JSONRenderer(indent=2)
+    ],
+    context_class=dict,
+    logger_factory=LoggerFactory())
 
 
 def includeme(config):
     config.add_request_method(
-        logger_factory,
+        lambda r: structlog.get_logger(),
         'logger',
         reify=True)
--- app/tweens/logging_tween.py
+++ app/tweens/logging_tween.py
@@ -1,5 +1,7 @@
 from timeit import default_timer
 
+from traceback import format_exception
+
 MESSAGE_TEMPLATE = 'HTTP {request_method} {request_path} responded {status_code} in {elapsed} ms'
 
 class logging_tween_factory(object):
@@ -8,31 +10,36 @@ class logging_tween_factory(object):
         self.registry = registry
 
     def __call__(self, request):
+        logger = request.logger
+
+        logger = logger.bind(
+            http_user_agent=request.headers.environ['HTTP_USER_AGENT'],
+            server_protocol=request.environ['SERVER_PROTOCOL'],
+            request_method=request.method,
+            request_url=request.url,
+            request_host=request.host,
+            request_path=request.path,
+            request_query_string=request.query_string,
+            referer=request.referer)
         start_time = default_timer()
 
         response = self.handler(request)
 
         end_time = 1000*(default_timer() - start_time)
 
-        request.logger.bind(
-            )
+        logger = logger.bind(
+            elapsed='{:.4f}'.format(end_time),
+            status_code=response.status_int,
+            content_type=response.content_type,
+            matched_route_name=request.matched_route.name if request.matched_route is not None else None,
+            matched_route_pattern=request.matched_route.pattern if request.matched_route is not None else None)
 
         if request.exception:
-            request.logger.error(
+            etype, value, tb = request.exc_info
+            logger.error(
                 MESSAGE_TEMPLATE,
-                exc_info=request.exc_info,
-                elapsed='{:.4f}'.format(end_time),
-                status_code=response.status_int,
-                content_type=response.content_type,
-                matched_route_name=request.matched_route.name if request.matched_route is not None else None,
-                matched_route_pattern=request.matched_route.pattern if request.matched_route is not None else None)
+                exc_info=format_exception(etype, value, tb))
         else:
-            request.logger.info(
-                MESSAGE_TEMPLATE,
-                elapsed='{:.4f}'.format(end_time),
-                status_code=response.status_int,
-                content_type=response.content_type,
-                matched_route_name=request.matched_route.name if request.matched_route is not None else None,
-                matched_route_pattern=request.matched_route.pattern if request.matched_route is not None else None)
+            logger.info(MESSAGE_TEMPLATE)
 
         return response
