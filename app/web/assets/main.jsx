56e8182386c4f0f3398c3da51802be0a11bbaef8
cheng93 <derek.c@hotmail.co.uk>
2017-05-25 19:57:26 +0100

    use ReactDOM import

--- app/web/assets/main.jsx
+++ app/web/assets/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import { App } from './app/app.component';
 
 
-render(
+ReactDOM.render(
   <App />,
   document.getElementById('app')
 );
