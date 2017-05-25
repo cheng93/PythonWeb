8623805a4040fe214e879cc11cbd522ab5d8ffec
cheng93 <derek.c@hotmail.co.uk>
2017-05-25 22:23:24 +0100

    add static view

--- app/web/__init__.py
+++ app/web/__init__.py
@@ -2,4 +2,5 @@ def includeme(config):
     config.include('.api')
     config.include('.index')
     config.add_route('throw_exception', pattern='/exception')
+    config.add_static_view(name='static', path='static')
     config.scan('.')
