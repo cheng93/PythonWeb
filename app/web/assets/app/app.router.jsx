b1dbd4e95c065e3c7c54abac2815167f7fd1f817
cheng93 <derek.c@hotmail.co.uk>
2017-05-27 21:26:21 +0100

    films page
    a films page, which is essentially a top level container
    add FilmsPage to route

--- app/web/assets/app/app.router.jsx
+++ app/web/assets/app/app.router.jsx
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import { ConnectedRouter } from 'react-router-redux'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-import { Film } from '../film/film.component';
+import { FilmsPage } from '../films/films.page';
 
 
 export function AppRouter(props) {
   return (
     <ConnectedRouter history={props.history}>
-      <Route exact path="/" component={Film}/>
+      <Route exact path="/" component={FilmsPage}/>
     </ConnectedRouter>
   );
 }
