23feda9b6576bd90556a9b1d3c7c680ae66da211
cheng93 <derek.c@hotmail.co.uk>
2017-05-28 15:36:59 +0100

    remove filmsTable container and moved redux to films page

--- app/web/assets/films/films.page.jsx
+++ app/web/assets/films/films.page.jsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-import { FilmsTableContainer } from './filmsTable/filmsTable.container';
+import { Layout, Panel } from 'react-toolbox/lib/layout';
 
-export function FilmsPage() {
-    return (
-        <FilmsTableContainer />
-    );
+import { Loading } from '../common/loading.component';
+
+import { fetchFilmsTable } from './filmsTable/filmsTable.actions';
+import { FilmsTable } from './filmsTable/filmsTable.component';
+
+
+function mapStateToProps(state) {
+    return {
+        films: state.FilmsTableReducer.items,
+        isLoading: state.FilmsTableReducer.isLoading,
+        loadingValue: state.FilmsTableReducer.loadingValue
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        onLoad: () => dispatch(fetchFilmsTable())
+    }
+}
+
+@connect(mapStateToProps, mapDispatchToProps)
+export class FilmsPage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        this.props.onLoad();
+    }
+
+    render() {
+        return (
+            <Layout>
+                <Panel>
+                    <div style={{ display: 'flex', justifyContent: 'center' }}>
+                        { this.props.isLoading
+                            ? <Loading value={this.props.loadingValue} />
+                            : 'Loaded'
+                        }
+                    </div>
+                </Panel>
+            </Layout>
+        );
+    }
 }
--- app/web/assets/films/filmsTable/filmsTable.component.jsx
+++ app/web/assets/films/filmsTable/filmsTable.component.jsx
@@ -6,13 +6,9 @@ export class FilmsTable extends Component {
         super(props);
     }
 
-    componentDidMount() {
-        this.props.onLoad();
-    }
-
     render() {
         return(
-            <p>{JSON.stringify(this.props.rows)}</p>
+            <p>{JSON.stringify(this.props.films)}</p>
         );
     }
 }
