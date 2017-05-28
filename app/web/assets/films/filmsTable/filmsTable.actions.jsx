08aff3895b3949458d32a09670f82ac2e307d134
cheng93 <derek.c@hotmail.co.uk>
2017-05-28 13:27:28 +0100

    progress tracker during axios get

--- app/web/assets/films/filmsTable/filmsTable.actions.jsx
+++ app/web/assets/films/filmsTable/filmsTable.actions.jsx
@@ -4,9 +4,10 @@ import axios from 'axios';
 export const FILMS_TABLE_LOADING = 'FILMS_TABLE_LOADING';
 export const FILMS_TABLE_RECEIVED = 'FILMS_TABLE_RECEIVED';
 
-export function loadingFilmsTable() {
+export function loadingFilmsTable(progress) {
     return {
-        type: FILMS_TABLE_LOADING
+        type: FILMS_TABLE_LOADING,
+        progress
     };
 }
 
@@ -21,7 +22,13 @@ export function fetchFilmsTable() {
     const endpoint = 'api/films';
 
     return (dispatch) => {
-        dispatch(loadingFilmsTable());
+        let config = {
+            onDownloadProgress: progressEvent => {
+                let percentCompleted = Math.floor((progressEvent.loaded * 100) / progressEvent.total);
+                dispatch(loadingFilmsTable(percentCompleted));
+            }
+        };
+        dispatch(loadingFilmsTable(0));
         return axios.get(endpoint)
             .then(films => dispatch(receivedFilmsTable(films.data)));
     };
