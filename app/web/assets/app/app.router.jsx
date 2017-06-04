5b83763419a6d2c0b5e31555b089bb4c63804bd8
cheng93 <derek.c@hotmail.co.uk>
2017-06-04 13:28:41 +0100

    move layout and panel to app router

--- app/web/assets/app/app.router.jsx
+++ app/web/assets/app/app.router.jsx
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import { ConnectedRouter } from 'react-router-redux'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
+import { Layout, Panel } from 'react-toolbox/lib/layout';
+
 import { FilmsPage } from '../films/films.page';
 
 
 export function AppRouter(props) {
   return (
     <ConnectedRouter history={props.history}>
-      <Route exact path="/" component={FilmsPage}/>
+      <Layout>
+        <Panel>
+          <Route exact path="/" component={FilmsPage} />
+        </Panel>
+      </Layout>
     </ConnectedRouter>
   );
 }
