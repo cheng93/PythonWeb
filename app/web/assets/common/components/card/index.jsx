faa4dac29698352a1f4cd603ae9c490b089e42d8
cheng93 <derek.c@hotmail.co.uk>
2017-06-07 20:24:19 +0100

    card components

---
+++ app/web/assets/common/components/card/index.jsx
@@ -0,0 +1,7 @@
+import { CardComponent } from './card.component';
+import { CardTitleComponent } from './cardTitle.component';
+import { CardTextComponent } from './cardText.component';
+
+export const Card = CardComponent;
+export const CardTitle = CardTitleComponent;
+export const CardText = CardTextComponent;
