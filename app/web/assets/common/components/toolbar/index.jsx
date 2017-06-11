aedee0fe7f0aa6388e93cfee852514013b8f8d21
cheng93 <derek.c@hotmail.co.uk>
2017-06-11 15:00:07 +0100

    index for toolbars

---
+++ app/web/assets/common/components/toolbar/index.jsx
@@ -0,0 +1,7 @@
+import { ToolbarComponent } from './toolbar.component';
+import { ToolBarRowComponent } from './toolbarRow.component';
+import { ToolbarSectionComponent } from './toolbarSection.component';
+
+export const Toolbar = ToolbarComponent;
+export const ToolbarRow = ToolBarRowComponent;
+export const ToolbarSection = ToolbarSectionComponent;
