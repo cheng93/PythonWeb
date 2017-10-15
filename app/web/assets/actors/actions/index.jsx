76f40a4246272575d82b7c10fc49bdf445665d3a
cheng93 <derek.c@hotmail.co.uk>
2017-10-15 14:00:51 +0100

    get actor films actions

--- app/web/assets/actors/actions/index.jsx
+++ app/web/assets/actors/actions/index.jsx
@@ -5,6 +5,12 @@ export {
 } from './getActor.actions';
 
 export {
+    GET_ACTOR_FILM_LIST_LOADING,
+    GET_ACTOR_FILM_LIST_RECEIVED,
+    fetchActorFilms
+} from './getActorFilmList.actions';
+
+export {
     GET_ACTOR_LIST_LOADING,
     GET_ACTOR_LIST_RECEIVED,
     fetchActors
