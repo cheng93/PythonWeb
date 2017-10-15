c031cc47d5a7f280d05cbc1f0414bc12b61dacf1
cheng93 <derek.c@hotmail.co.uk>
2017-10-15 12:59:35 +0100

    replace import with export

--- app/web/assets/app/app.router.jsx
+++ app/web/assets/app/app.router.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { ConnectedRouter } from 'react-router-redux'
 import { Route, Switch } from 'react-router-dom';
 
-import { FilmList, Router as FilmsRouter } from '../films';
+import { FilmListPage, FilmsRouter } from '../films';
 
 
 export function AppRouter(props) {
   return (
     <ConnectedRouter history={props.history}>
         <Switch>
-            <Route exact path="/" component={FilmList} />
+            <Route exact path="/" component={FilmListPage} />
             <Route path="/films" component={FilmsRouter} />
         </Switch>
     </ConnectedRouter>
