a5ffbe9a68847adb23bb3675bfa50be0a691f0d4
cheng93 <derek.c@hotmail.co.uk>
2017-10-15 13:48:30 +0100

    get actor actions

---
+++ app/web/assets/actors/actions/getActor.actions.jsx
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+
+export const GET_ACTOR_LOADING = 'GET_ACTOR_LOADING';
+export const GET_ACTOR_RECEIVED = 'GET_ACTOR_RECEIVED';
+
+export function loadingActor(progress) {
+    return {
+        type: GET_ACTOR_LOADING,
+        progress
+    };
+}
+
+export function receivedActor(actor) {
+    return {
+        type: GET_ACTOR_RECEIVED,
+        actor
+    };
+}
+
+export function fetchActor(id) {
+    const endpoint = `/api/actor/${id}`;
+
+    return (dispatch) => {
+        let config = {
+            onDownloadProgress: progressEvent => {
+                let percentCompleted = Math.floor((progressEvent.loaded * 100) / progressEvent.total);
+                dispatch(loadingActor(percentCompleted));
+            }
+        };
+        dispatch(loadingActor(0));
+        return axios.get(endpoint, config)
+            .then(async actor => {
+                dispatch(receivedActor(actor.data))
+            });
+    };
+}
--- app/web/assets/actors/actions/index.jsx
+++ app/web/assets/actors/actions/index.jsx
@@ -1,4 +1,10 @@
 export {
+    GET_ACTOR_LOADING,
+    GET_ACTOR_LIST_RECEIVED,
+    fetchActor
+} from './getActor.actions';
+
+export {
     GET_ACTOR_LIST_LOADING,
     GET_ACTOR_LIST_RECEIVED,
     fetchActors
