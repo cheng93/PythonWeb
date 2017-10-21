e47c7b480f5ba45742f66acd8fa0b9525159e861
cheng93 <derek.c@hotmail.co.uk>
2017-10-21 21:10:51 +0100

    better way of RedirectTo

--- app/web/assets/app/app.router.jsx
+++ app/web/assets/app/app.router.jsx
@@ -3,14 +3,14 @@ import { ConnectedRouter } from 'react-router-redux'
 import { Route, Redirect, Switch } from 'react-router-dom';
 
 import { ActorsRouter } from '../actors';
-import { FilmListPage, FilmsRouter } from '../films';
+import { FilmsRouter } from '../films';
 
 
 export function AppRouter(props) {
   return (
     <ConnectedRouter history={props.history}>
         <Switch>
-            <Route exact path="/" render={() => { return (<Redirect to='/films' />); }} />
+            <Redirect exact from="/" to="/films" />
             <Route path="/actors" component={ActorsRouter} />
             <Route path="/films" component={FilmsRouter} />
         </Switch>
