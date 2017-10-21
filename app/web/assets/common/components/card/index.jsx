8ec63d0fbfa3fdfd2c4a073fa9ce21e55f7e5361
cheng93 <derek.c@hotmail.co.uk>
2017-10-21 15:38:01 +0100

    move styles from card to page

--- app/web/assets/common/components/card/index.jsx
+++ app/web/assets/common/components/card/index.jsx
@@ -1,7 +1,6 @@
 import { CardComponent } from './card.component';
 import { CardTitleComponent } from './cardTitle.component';
 import { CardTextComponent } from './cardText.component';
-import './card.scss';
 
 export const Card = CardComponent;
 export const CardTitle = CardTitleComponent;
