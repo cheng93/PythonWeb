27c2ee742f645108cf68111c219f99b3a6cef8b2
cheng93 <derek.c@hotmail.co.uk>
2017-11-15 20:27:13 +0000

    rest of toolbar tests

---
+++ app/web/assets/tests/dvdrental/common/components/toolbar/toolbarSection.test.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { ToolbarSection } from '../../../../../src/dvdrental/common/components/toolbar';
+
+describe('ToolbarSection', () => {
+    test('should render <section>', () => {
+        const wrapper = shallow(<ToolbarSection />);
+
+        expect(wrapper.find("section")).toHaveLength(1);
+    });
+
+    test('should have mdc-toolbar__section class', () => {
+        const wrapper = shallow(<ToolbarSection />);
+
+        expect(wrapper.first().hasClass("mdc-toolbar__section")).toBe(true);
+    });
+
+    test('should render children', () => {
+        var text = "Hello World";
+        const wrapper = shallow(<ToolbarSection>{text}</ToolbarSection>);
+
+        expect(wrapper.contains(text)).toBe(true);
+    });
+
+    describe('has align', () => {
+        test('should have mdc-toolbar__section--align-start class', () => {
+            const wrapper = shallow(<ToolbarSection align='start' />);
+    
+            expect(wrapper.first().hasClass("mdc-toolbar__section--align-start")).toBe(true);
+        });
+
+        test('should have mdc-toolbar__section--align-end class', () => {
+            const wrapper = shallow(<ToolbarSection align='end' />);
+    
+            expect(wrapper.first().hasClass("mdc-toolbar__section--align-end")).toBe(true);
+        });
+        
+        test('should not have mdc-toolbar__section--align-* class', () => {
+            const wrapper = shallow(<ToolbarSection align='invalid' />);
+    
+            expect(wrapper.first().hasClass("mdc-toolbar__section--align-invalid")).toBe(false);
+        });
+    });
+});
