1cc7d0de470ecef623929246c4e901884225de9b
cheng93 <derek.c@hotmail.co.uk>
2017-11-16 20:26:13 +0000

    give gridlist a tile aspect prop

--- app/web/assets/src/dvdrental/common/components/gridList/gridList.component.jsx
+++ app/web/assets/src/dvdrental/common/components/gridList/gridList.component.jsx
@@ -5,11 +5,32 @@ export class GridListComponent extends Component {
         super(props);
     }
 
+    getClass() {
+        const { tileAspect } = this.props;
+
+        let className = 'mdc-grid-list';
+
+        let aspects = [
+            '1x1',
+            '16x9',
+            '2x3',
+            '3x2',
+            '4x3',
+            '3x4'
+        ];
+
+        if (aspects.some(a => a === tileAspect)) {
+            className += ` mdc-grid-list--tile-aspect-${tileAspect}`;
+        }
+
+        return className;
+    };
+
     render() {
         const { children } = this.props;
 
         return (
-            <div className='mdc-grid-list mdc-grid-list--tile-aspect-1x1'>
+            <div className={this.getClass()}>
                 <ul className='mdc-grid-list__tiles'>
                     {children}
                 </ul>
