3d0693fdeec0e375940f099b5465792c53316640
cheng93 <derek.c@hotmail.co.uk>
2018-02-03 21:50:37 +0000

    home and player components

---
+++ app/web/assets/src/fof/components/home/home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <section>
+            <h2 class='mdc-typography--title'>Teams</h2>
+        </section>
+    </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
