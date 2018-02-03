b3b32da76cd60e58742ee4329e889d7e42c31e0c
cheng93 <derek.c@hotmail.co.uk>
2018-02-03 21:49:48 +0000

    get vuex store into app

--- app/web/assets/src/fof/main.js
+++ app/web/assets/src/fof/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import { AppRouter } from './app/app.router';
+import store from './store';
 import appVue from './app/app.vue';
 
 Vue.use(VueRouter);
