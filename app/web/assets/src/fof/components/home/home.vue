6f79bd6abd1716979931cc56b6ec997ab2e1920c
cheng93 <derek.c@hotmail.co.uk>
2018-02-04 00:32:11 +0000

    Home page renders list of teams

--- app/web/assets/src/fof/components/home/home.vue
+++ app/web/assets/src/fof/components/home/home.vue
@@ -2,13 +2,18 @@
     <div>
         <section>
             <h2 class='mdc-typography--title'>Teams</h2>
+            <home-teams />
         </section>
     </div>
 </template>
 
 <script>
+import homeTeams from './homeTeams.vue'
+
 export default {
-  
+    components: {
+        homeTeams
+    }
 }
 </script>
 
---
+++ app/web/assets/src/fof/components/home/homeTeams.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <ul v-for='team in teams' :key='team.teamId'>
+            <li>{{team.city}} {{team.teamName}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+    computed: { 
+        ...mapGetters({
+            teams: 'getTeams'
+        }),
+    },
+    beforeCreate() {
+        this.$store.dispatch('getTeams')
+    }
+}
+</script>
