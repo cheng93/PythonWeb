ddbc739d66c40b76acbfb7c6e90c528e7d9d9e1a
cheng93 <derek.c@hotmail.co.uk>
2018-02-04 00:31:31 +0000

    restructure apollo due to removing vue-apollo

--- app/web/assets/src/fof/apollo/provider.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import VueApollo from 'vue-apollo'
-import client from './client'
-
-Vue.use(VueApollo)
-
-export default new VueApollo({
-    defaultClient: client
-})
--- app/web/assets/src/fof/main.js
+++ app/web/assets/src/fof/main.js
@@ -1,13 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import apolloProvider from './apollo/provider'
 import router from './router'
 import store from './store'
 import app from './components/app.vue'
 
 new Vue({
-    apolloProvider,
     router,
     store,
     el: '#app',
