abe62ebeff4d450cfa7ad69fda81bc60f039d0ea
cheng93 <derek.c@hotmail.co.uk>
2018-02-05 03:29:58 +0000

    add mdc-typography to main

--- app/web/assets/src/fof/components/common/page.vue
+++ app/web/assets/src/fof/components/common/page.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <toolbar :title=title />
-        <main class="mdc-toolbar-fixed-adjust mdc-layout-grid">
+        <main class="mdc-toolbar-fixed-adjust mdc-layout-grid mdc-typography">
             <div class="mdc-layout-grid__inner">
                 <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-2-desktop mdc-layout-grid__cell--span-1-tablet"/>
                 <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-8-desktop mdc-layout-grid__cell--span-7-tablet">
