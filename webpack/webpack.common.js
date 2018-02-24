75a4fff94f5509a6a806fa8889125175e39d2da7
cheng93 <derek.c@hotmail.co.uk>
2018-02-24 21:21:18 +0000

    use @material scss through webpack

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -61,7 +61,12 @@ module.exports = {
                 loader: 'css-loader'
               },
               {
-                loader: 'sass-loader'
+                loader: 'sass-loader',
+                options: {
+                  includePaths: [
+                    path.resolve(__dirname, '../node_modules')
+                  ]
+                }
               }
             ]
         })
