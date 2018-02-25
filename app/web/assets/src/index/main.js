4b4261ef353b66a381a8285a44d08635ddd89833
cheng93 <derek.c@hotmail.co.uk>
2018-02-25 16:25:00 +0000

    index styles and image

--- app/web/assets/src/index/main.js
+++ app/web/assets/src/index/main.js
@@ -1,2 +1,12 @@
 import '../app.scss';
 import './main.scss';
+
+import '../img/angular.svg';
+import '../img/ansible.svg';
+import '../img/csharp.svg';
+import '../img/docker.svg';
+import '../img/elasticsearch.svg';
+import '../img/git.svg';
+import '../img/microsoft-azure.svg';
+import '../img/microsoft-sql-server.svg';
+import '../img/vue.svg';
