78720b0d1e67375c0e2603a0592711c9c43bbb61
cheng93 <derek.c@hotmail.co.uk>
2018-03-03 15:39:05 +0000

    fof common: added subheader component

---
+++ app/web/assets/src/fof/components/common/pageSubheader.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="mdc-layout-grid">
+        <div class="mdc-layout-grid__inner">
+            <div class="text-center mdc-layout-grid__cell mdc-layout-grid__cell--span-12 mdc-theme--primary">
+                <h1 class="mdc-typography--display2 mt-3 mb-1">
+                    {{header}}
+                </h1>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        header: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
