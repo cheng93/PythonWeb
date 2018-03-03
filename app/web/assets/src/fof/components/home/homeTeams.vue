aa1ecdc3a3741fb5f006a36472c410dcdc4daa27
cheng93 <derek.c@hotmail.co.uk>
2018-03-03 15:09:57 +0000

    fof home: use pageSection component

--- app/web/assets/src/fof/components/home/homeTeams.vue
+++ app/web/assets/src/fof/components/home/homeTeams.vue
@@ -1,26 +1,21 @@
 <template>
-    <section>
-        <div class="mdc-layout-grid">
-            <div class="mdc-layout-grid__inner">
-                <div class="mdc-card mdc-layout-grid__cell mdc-layout-grid__cell--span-6-desktop mdc-layout-grid__cell--span-6-tablet mdc-layout-grid__cell--span-4-phone grid-start-4-desktop grid-start-2-tablet">
-                    <h1 class="mdc-typography--display1 mb-3 mdc-theme--primary ml-5 mt-5">Teams</h1>
-                    <div class="mdc-layout-grid mx-5 mb-4">
-                        <ul class="mdc-list mdc-list--avatar-list mdc-layout-grid__inner">
-                            <home-team-item v-for='team in teams' :key='team.teamId' 
-                                :team=team tag="li" />
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    <page-section :header=header>
+        <ul class="mdc-list mdc-list--avatar-list mdc-layout-grid__inner">
+            <home-team-item v-for='team in teams' :key='team.teamId' 
+                :team=team tag="li" />
+        </ul>
+    </page-section>
 </template>
 
 <script>
 import homeTeamItem from './homeTeamItem'
+import pageSection from '../common/pageSection'
 
 export default {
-    computed: { 
+    computed: {
+        header() {
+            return 'Teams'
+        },
         teams() { 
             return this.$store.getters.getTeams.sort((x,y) => x.teamName.localeCompare(y.teamName))
         }
@@ -29,7 +24,8 @@ export default {
         this.$store.dispatch('getTeams')
     },
     components: {
-        homeTeamItem
+        homeTeamItem,
+        pageSection
     }
 }
 </script>
