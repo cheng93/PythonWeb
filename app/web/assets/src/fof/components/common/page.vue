f0e94956f8deea3911c7e93afb234c5b727a4936
cheng93 <derek.c@hotmail.co.uk>
2018-03-03 13:41:47 +0000

    fof: adjust home layout

--- app/web/assets/src/fof/components/common/page.vue
+++ app/web/assets/src/fof/components/common/page.vue
@@ -1,13 +1,8 @@
 <template>
-    <div>
+    <div class="mdc-typography">
         <toolbar :title=title />
-        <main class="mdc-toolbar-fixed-adjust mdc-layout-grid mdc-typography">
-            <div class="mdc-layout-grid__inner">
-                <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-2-desktop mdc-layout-grid__cell--span-1-tablet"/>
-                <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-8-desktop mdc-layout-grid__cell--span-7-tablet">
-                    <slot />
-                </div>
-            </div>
+        <main class="mdc-toolbar-fixed-adjust">
+            <slot />
         </main>
     </div>
 </template>
--- app/web/assets/src/fof/components/home/homeTeams.vue
+++ app/web/assets/src/fof/components/home/homeTeams.vue
@@ -1,13 +1,22 @@
 <template>
-    <section class="mdc-layout-grid">
-        <h2 class='mdc-typography--title'>Teams</h2>
-        <ul class="mdc-list mdc-layout-grid__inner">
-            <router-link v-for='team in teams' :key='team.teamId' 
-                :to="'/teams/' + team.teamId" tag="li"
-                class="mdc-list-item mdc-layout-grid__cell mdc-layout-grid__cell--span-4">
-                    {{team.teamName}}
-            </router-link>
-        </ul>
+    <section>
+        <div class="mdc-layout-grid">
+            <div class="mdc-layout-grid__inner">
+                <div class="mdc-card mdc-layout-grid__cell mdc-layout-grid__cell--span-6-desktop mdc-layout-grid__cell--span-6-tablet mdc-layout-grid__cell--span-4-phone grid-start-4-desktop grid-start-2-tablet">
+                    <h1 class="mdc-typography--display1 mb-3 mdc-theme--primary ml-5 mt-5">Teams</h1>
+                    <div class="mdc-layout-grid mx-5 mb-4">
+                        <ul class="mdc-list mdc-list--avatar-list mdc-layout-grid__inner">
+                            <router-link v-for='team in teams' :key='team.teamId' 
+                                :to="'/teams/' + team.teamId" tag="li"
+                                class="pl-0 link mdc-list-item mdc-layout-grid__cell mdc-layout-grid__cell--span-4">
+                                    <span class="mdc-list-item__graphic mdc-theme--secondary-bg mdc-theme--text-primary-on-secondary">DC</span>
+                                    {{team.teamName}}
+                            </router-link>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 </template>
 
