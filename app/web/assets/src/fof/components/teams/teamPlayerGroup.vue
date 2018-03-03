b1d7b70ce0d6def2411e73fb2a09ed5de34bcbd3
cheng93 <derek.c@hotmail.co.uk>
2018-03-03 20:46:32 +0000

    fof: style group name element

--- app/web/assets/src/fof/components/teams/teamPlayerGroup.vue
+++ app/web/assets/src/fof/components/teams/teamPlayerGroup.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-12-desktop mdc-layout-grid__cell--span-8-tablet mdc-layout-grid__cell--span-4-phone">
-        <h2 class="mdc-typography--headline">{{name}}</h2>
+        <h2 class="my-1 mdc-typography--headline">{{name}}</h2>
         <div class="mdc-layout-grid">
             <ul class="mdc-list mdc-list--avatar-list mdc-list--two-line mdc-layout-grid__inner">
                 <team-player-group-item v-for="player in players" :key=player.id :player=player />
