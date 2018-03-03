cb28d2960cbe8c6e8b079d3ac6ab3ceba0358a1c
cheng93 <derek.c@hotmail.co.uk>
2018-03-03 19:42:15 +0000

    fof: use listInitialGraphic

--- app/web/assets/src/fof/components/home/homeTeamItem.vue
+++ app/web/assets/src/fof/components/home/homeTeamItem.vue
@@ -1,11 +1,13 @@
 <template>
     <router-link tag="li" :to=url
         class="pl-0 link mdc-list-item mdc-layout-grid__cell mdc-layout-grid__cell--span-4">
-        <span class="mdc-list-item__graphic mdc-theme--secondary-bg mdc-theme--text-primary-on-secondary">{{initials}}</span>
+        <list-initial-graphic :text=fullname />
         <span class="mdc-typography--body1">{{team.teamName}}</span>
     </router-link>
 </template>
 <script>
+import listInitialGraphic from '../common/listInitialGraphic'
+
 export default {
     props: {
         team: {
@@ -17,13 +19,12 @@ export default {
         url() {
             return `/teams/${this.team.teamId}`
         },
-        initials() {
-            let fullName = `${this.team.city} ${this.team.teamName}`
-            return fullName
-                .split(' ')
-                .map(x => x[0])
-                .join('')
+        fullname() {
+            return `${this.team.city} ${this.team.teamName}`
         }
+    },
+    components: {
+        listInitialGraphic
     }
 }
 </script>
