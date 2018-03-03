f9b22cfb07434ada5c0acb1e9c01cc27add9ad8a
cheng93 <derek.c@hotmail.co.uk>
2018-03-03 22:56:07 +0000

    fof: added link on team standing

--- app/web/assets/src/fof/components/teams/teamStandings.vue
+++ app/web/assets/src/fof/components/teams/teamStandings.vue
@@ -1,21 +1,25 @@
 <template>
     <page-section :header=header>
-        <table class="mdl-data-table mdl-js-data-table">
-            <thead>
-                <tr>
-                    <th>Year</th>
-                    <th class="mdl-data-table__cell--non-numeric">Standing</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for='standing in ordered' :key='standing.year'>
-                    <td>{{standing.year}}</td>
-                    <td class="mdl-data-table__cell--non-numeric">
-                        {{standing.standing}}
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+        <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-12-desktop mdc-layout-grid__cell--span-8-tablet">
+            <table class="full-width mdl-data-table mdl-js-data-table">
+                <thead>
+                    <tr class="mdc-typography--headline">
+                        <th class="py-2">Year</th>
+                        <th class="py-2 mdl-data-table__cell--non-numeric">Standing</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for='standing in ordered' :key='standing.year'>
+                        <router-link :to=standing.url class="link" :tag="'td'">
+                            {{standing.year}}
+                        </router-link>
+                        <router-link :to=standing.url class="link mdl-data-table__cell--non-numeric" :tag="'td'">
+                            {{standing.standing}}
+                        </router-link>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </page-section>
 </template>
 
